c2a20bcc7cbcb398cea33aefd629bab676083a93
Mark Strickland <mstrickland@globys.com>
2019-03-29 10:16:36 -0700

    Add first successful build after npm install.

---
+++ spec/doctor-lookup-spec.js
@@ -0,0 +1,11 @@
+import DoctorLookup from ''./../src/doctorlookup.js';
+
+describe('DoctorLookup', function() {
+  it('should', function()) {
+
+  let doctorLookup = new DoctorLookup();
+  
+  expect(doctorLookup).toEqual();
+});
+
+}
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+import $ from 'jquery';
+//import { DoctorLookup } from './doctor-lookup.js';
+
+$(document).ready(function() {
+//  let doctorLookup = new DoctorLookup();
+  $("#Hello").text("Hello");
+});
