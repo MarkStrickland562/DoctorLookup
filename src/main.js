cd33616fe3287bccd88be557b4870c8895511240
Mark Strickland <mstrickland@globys.com>
2019-03-29 15:00:14 -0700

    Add forEach loops for nested arrays.

--- src/main.js
+++ src/main.js
@@ -8,7 +8,12 @@ $(document).ready(function() {
   $("#search-form").submit(function(event){
     event.preventDefault();
 
+    $("#doctor-info").hide();
+    $("#searching").show();
+
     let searchString = "";
+    let tableText = "";
+    let phone = "";
 
     if ($('#searchFirstName').val() != "") {
       searchString = "?first_name=" + $('#searchFirstName').val();
@@ -30,9 +35,32 @@ $(document).ready(function() {
     let promise = doctorLookup.getDoctorInfo(searchString);
     promise.then(function(response) {
       let body = JSON.parse(response);
+      var table = $('#doctor-info');
+
+      table.append('<thead><tr><th>First Name</th><th>Last Name</th><th>Phone Number(s)</th><th>Website</th><th>Accepting Patients?</th></tr></thead>');
+          body.data.forEach(function(doctor) {
+            tableText = '<tr><td>' + doctor.profile.first_name + '</td>' +
+                             '<td>' + doctor.profile.last_name + '</td>';
+            phone = '<td>';
+            doctor.practices.forEach(function(practice) {
+              phone += practice.phones[0].number + ' ';
+            });
+            phone += '</td>';
+            tableText += phone;
+//                             '<td>' + doctor.practices[0].website + '</td>' +
+//                             '<td>' + doctor.practices[0].accepts_new_patients + '</td>' +
+            tableText += '</tr>';
+            table.append(tableText);
+doctor.practices.forEach(function(practice) {
+console.log(doctor.profile.first_name + ' ' + doctor.uid);
+console.log(practice.phones);
+});
+          });
+          $("#searching").hide();
+          $("#doctor-info").show();
 
 // console.log(body.data[0].profile.first_name);
- console.log(body.data[0].profile.last_name);
+// console.log(body.data[0].profile.last_name);
 // console.log(body.data[0].practices[0].visit_address.street);
 // console.log(body.data[0].practices[0].visit_address.city);
 // console.log(body.data[0].practices[0].visit_address.state);
